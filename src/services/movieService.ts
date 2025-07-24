554d0477f56b7fdf947797ff584b597388412da3
Iryna_Novikova <iris_vs@ukr.net>
2025-07-24 22:47:49 +0300

    services fix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import type { Movie } from "../types/movie"; 
-
-axios.defaults.baseURL = 'https://api.themoviedb.org/3/search';
-axios.defaults.headers["Authorization"] = `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`;
     
 interface MoviesHttpResponse {
     results: Movie[];
 }
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
+    
+    axios.defaults.baseURL = 'https://api.themoviedb.org/3/search';
+    axios.defaults.headers["Authorization"] = `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`;
+    
     const endPoint = '/movie';
     const params = {
         query,
